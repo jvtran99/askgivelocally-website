1be90fd5e2935602f9971490968251d45654de6d
John Tran <john.jvtran@gmail.com>
2020-07-13 22:03:57 -0500

    responsive layout, grid icons

--- lib/screens/landingpage.dart
+++ lib/screens/landingpage.dart
@@ -8,87 +8,250 @@ class LandingPage extends StatelessWidget {
         title: Text('AskGiveLocally 1'),
       ),
       body: Container(
-        color: Theme.of(context).primaryColor,
+        // color: Theme.of(context).primaryColor,
+        color: Colors.white,
         child: Center(
           // widthFactor: 900,
           child: SingleChildScrollView(
             child: Container(
-              width: 1020,
+              // width: 1020,
               child: Column(
                 children: [
                   Container(
                     color: Colors.amber,
-                    height: 300,
+                    height: 600,
                     width: double.infinity,
-                  ),
-                  Text('Testing'),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    color: Colors.green[300],
-                    height: 300,
-                    width: double.infinity,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [Text('Simple steps to get started')],
-                    ),
+                    // width: MediaQuery.of(context).size.width * 1,
                   ),
                   Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    padding: EdgeInsets.all(10),
-                    color: Colors.redAccent,
-                    height: 200,
-                    width: double.infinity,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    width: 1020,
+                    child: Column(
                       children: [
                         Container(
-                          alignment: Alignment.topLeft,
-                          color: Colors.green,
-                          child: Text('Simple steps to get started'),
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          padding: EdgeInsets.all(10),
+                          color: Colors.green[300],
+                          height: 400,
+                          width: double.infinity,
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Column(
+                                  children: [
+                                    Text('Simple steps to get started'),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                  flex: 2,
+                                  child: Center(child: Text('Insert Image'))),
+                            ],
+                          ),
                         ),
                         Container(
-                          color: Colors.red,
-                          child: Text(' '),
-                        )
+                          padding: EdgeInsets.all(10),
+                          color: Colors.red[300],
+                          height: 500,
+                          width: double.infinity,
+                          child: Row(
+                            // crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                flex: 4,
+                                child: Center(child: Text('Insert Image')),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Container(
+                                    // color: Colors.pink,
+                                    ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  'Now more simple, secure, and faster than ever - with geolocation built-in. Now more simple, secure, and faster than ever - with geolocation built-in',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Container(
+                                    // color: Colors.blue,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          height: 300,
+                          color: Colors.blue,
+                          width: double.infinity,
+                          child: Row(
+                            // crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Center(
+                                  child: Text(
+                                      'Millions of giving opportunity. No credit card needed. '),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Center(child: Text('Insert Image')),
+                              ),
+                            ],
+                          ),
+                        ),
+                        FlatButton(
+                          padding: EdgeInsets.all(30),
+                          onPressed: () {},
+                          child: Text('Get AskGiveLocally Free'),
+                          color: Theme.of(context).accentColor,
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(20.0)),
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                decoration: BoxDecoration(color: Colors.white),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 5,
+                              child: Container(
+                                margin: EdgeInsets.only(top: 10),
+                                padding: EdgeInsets.symmetric(
+                                  vertical: 18,
+                                  horizontal: 30,
+                                ),
+                                // decoration: BoxDecoration(color: Colors.white),
+                                child: Align(
+                                  // widthFactor: 4,
+                                  alignment: AlignmentDirectional.topCenter,
+                                  child: Wrap(
+                                    spacing: 95,
+                                    runSpacing: 60,
+                                    children: <Widget>[
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Icon(
+                                            Icons.face,
+                                            size: 60,
+                                          ),
+                                          Text('Face')
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Container(),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    color: Colors.redAccent,
-                    height: 200,
-                    width: double.infinity,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          color: Colors.green,
-                          child: Text(
-                              'Now more simple, secure, and faster than ever - with geolocation built-in'),
-                        ),
-                        Container(
-                          color: Colors.red,
-                          child: Text(
-                              'Now more simple, secure, and faster than ever - with geolocation built-in'),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    color: Colors.blue,
-                    width: double.infinity,
-                    child: Text(
-                        'Millions of giving opportunity. No credit card needed. '),
-                  ),
-                  FlatButton(
-                      padding: EdgeInsets.all(30),
-                      onPressed: () {},
-                      child: Text('Get AskGiveLocally Free'),
-                      color: Theme.of(context).accentColor,
-                      shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(20.0)))
                 ],
               ),
             ),
